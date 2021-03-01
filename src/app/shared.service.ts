309f3c7c45c84c63931e8b6e86a9fa6befdf4c58
neethurav <neethu03061995@gmail.com>
2021-03-01 08:55:29 +0530

    updated

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,14 +7,15 @@ import { Injectable } from '@angular/core';
 export class SharedService {
   
   private _persondetails:any={}
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   set persondetails(personObj:any){
     this._persondetails ={
       name: personObj.name,
       company: personObj.company,
       phone: personObj.phone,
-      email:personObj.email
+      email:personObj.email,
+      _id:personObj._id
     }
  
   }
@@ -22,4 +24,34 @@ export class SharedService {
     return this._persondetails;
   }
 
+
+  createthelist(input:any){
+    console.log(input);
+    const Options = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        
+      })
+    }
+    //    let body={
+    //     name:"gopika",
+    //     company: "right",    
+    //     phone:"+91 3q433434",
+    //     email:"g@gmail.com"
+    //    }
+   return this.http.post('http://localhost:3000/notes',input,Options);
+    
+   }
+
+deletemember(deleteid:string){
+ return this.http.delete('http://localhost:3000/notes/'+deleteid)
+
+}
+
+
+updatemember(input:any){
+  console.log("--------",input,this._persondetails._id);
+// return {"status":"200"};
+  // this.http.put('http://localhost:3000/notes/'+ this._persondetails._id,input)
+}
 }
